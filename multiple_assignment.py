13d139e6900e632cb5cc8e77b5398e72215863b9
Harsh <goyalharsh300@gmail.com>
2023-09-24 21:07:40 +0530

    new

---
+++ multiple_assignment.py
@@ -0,0 +1,15 @@
+#multiple assignment = allows us to assign multiple variable at the same time in one line of code
+
+#name = "Harsh"
+#age = 21
+#attractive= True
+
+name, age, attractive = "Harsh", 21, True
+
+spongebob = patrick = sandy = squidward = 30
+
+print(name)
+print(age)
+print(attractive)
+
+print(spongebob,patrick, sandy, squidward)
