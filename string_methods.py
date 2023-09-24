b7f277d609640de149de22b76202d5a5136fbd44
Harsh <goyalharsh300@gmail.com>
2023-09-24 21:23:44 +0530

    new

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+name = "harsh"
+
+print(len(name))  #lenght of the string
+print(name.find("r")) #find the first occurance of the string
+print(name.capitalize()) #Capitalize the first letter of string
+print(name.upper()) #converts whole string to upper case
+print(name.lower()) #coverts whole string to lower case
+print(name.isdigit()) #checks whether the string is number or not
+print(name.isalpha()) #checks whether the string has only alphabets or not
+print(name.count("h")) #counts the number of occurances of string
+print(name.replace("h","?")) #replaces a string with other string (what to change, what to replace)
+print(name*3) #print the number n number of times
