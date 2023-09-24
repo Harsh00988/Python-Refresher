c1e681a6b61da0b2147785a463e9948140ae9fb4
Harsh <goyalharsh300@gmail.com>
2023-09-24 20:53:34 +0530

    first commit

---
+++ variables.py
@@ -0,0 +1,18 @@
+#variable is container for a value.
+
+first_name = "Harsh"  #String
+last_name = "Goyal"
+full_name = first_name + last_name
+print("Hello" + full_name) 
+print(type(full_name))
+
+age = 22 #Integer
+age += 1
+print("age =", age)
+print(type(age))
+
+height = 281.4 #float
+print("My height is: "+str(height)+"cm")
+
+human = True #boolean
+print("Are you a human: " + str(human))
