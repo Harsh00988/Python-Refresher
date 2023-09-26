c3c6483ef5e0ccf15e29fa69c7b0a88750bb6509
Harsh <goyalharsh300@gmail.com>
2023-09-26 22:24:47 +0530

    new

---
+++ string-slicing.py
@@ -0,0 +1,24 @@
+#slicing - creates a substring by extracting elements from another string
+#           indexing[] or slice()
+#           [start:stop:step]
+
+# name = "Harsh Goyal"
+
+# fisrt_name = name[:5]
+# last_name = name[6:]
+# funky_name = name[::2]
+# reversed_name = name[::-1]
+
+# print(fisrt_name)
+# print(last_name)
+# print(funky_name)
+# print(reversed_name)
+
+
+#Now using slice function
+
+website = "http://google.com"
+
+slice = slice(7,-4)
+
+print(website[slice])
