17d2b3b85a022f7703c35fa08702fc6978b69766
Harsh <goyalharsh300@gmail.com>
2023-09-30 16:21:45 +0530

    new

---
+++ Learning/sets.py
@@ -0,0 +1,17 @@
+# sets = collection which is onorderedm unindexed. No duplicate Values
+
+utensils = {"fork","spoon","knife"}
+# s = {"fork", "money", "cup"}
+
+# utensils.add("napkin") #adds element to the set
+# utensils.remove("fork") #removes element from the set
+# utnsils.clear() #removes all elements from set
+# print(utensils.intersection(s)) #intersection of s and utnesils
+# utensils.update(s) #adds elements od s in utensils
+# dinner_table = utensils.union(s) - union of s and utensils 
+# utensils.difference(s) - difference of utensils and s
+
+for x in utensils:
+    print(x)
+
+
