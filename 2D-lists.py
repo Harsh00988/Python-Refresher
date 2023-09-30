060cb44ed700856c60fbd55f3ea34f378d778e1e
Harsh <goyalharsh300@gmail.com>
2023-09-30 15:57:31 +0530

    new

---
+++ 2D-lists.py
@@ -0,0 +1,9 @@
+# 2D list - list of lists 
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks,dinner,dessert]
+
+print(food[0][1]) #this is how you access an element in the list
