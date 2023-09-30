49363980837b11c1ac47615c778a52a41e04aad8
Harsh <goyalharsh300@gmail.com>
2023-09-30 15:07:15 +0530

    new commit

---
+++ Difficult questions/build-strings-using prestrings.py
@@ -0,0 +1,7 @@
+# You are given an array of strings A of size N and a string B. 
+# You have to return the minuimum number of preStrings required to be concatenated in any order to build B.
+# if it is not possible then return 1. 
+#preStrings is any prefix of a string present in a
+
+#A = ["abc","bcb","ka","efgh"]
+#B = "efkabc"
