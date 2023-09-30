d894cd980b0316e617caa1f097fc356bd65f7292
Harsh <goyalharsh300@gmail.com>
2023-09-30 17:13:55 +0530

    new

---
+++ Learning/index-operator.py
@@ -0,0 +1,9 @@
+# index operator [] - gives access to a sequence's element (str,list, tuples)
+
+name = "harsh goyal"
+
+# if(name[0].islower()):
+#     name = name.capitalize()
+# last_letter = name[-1] #negative indexing
+    
+print(name)
