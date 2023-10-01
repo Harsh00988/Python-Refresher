678026f42dc4ba07f35142670f46b4ed5e94bc76
Harsh <goyalharsh300@gmail.com>
2023-10-01 17:43:21 +0530

    new

---
+++ function.py
@@ -0,0 +1,7 @@
+# function - a block of code which is executed only when it is called
+def hello(first_name, last_name, age): #parameters
+    print(f"Hello {first_name} {last_name}")
+    print(f"You are {age} years old")
+    print("Have a nice day")
+    
+hello("Harsh", "Goyal", 21) #arguments
